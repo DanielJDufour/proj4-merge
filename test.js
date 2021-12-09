38b66bdfdd361b839629183969a498739e714752
DanielJDufour <daniel.j.dufour@gmail.com>
2021-12-09 18:22:49 -0500

    mvp

---
+++ test.js
@@ -0,0 +1,46 @@
+const test = require("flug");
+const proj4 = require("proj4");
+const proj4FullyLoaded = require("proj4-fully-loaded");
+
+const merge = require("./proj4-merge");
+
+const expectation = {
+  projName: "utm",
+  zone: 17,
+  datumCode: "WGS84",
+  units: "m",
+  no_defs: true,
+};
+
+test("merge zero", ({ eq }) => {
+  let msg;
+  try {
+    merge();
+  } catch (error) {
+    msg = error.message;
+  }
+  eq(msg, "[proj4-merge] merge called with zero instances of proj4");
+});
+
+test("merge of 1", ({ eq }) => {
+  const merged = merge(proj4);
+  // if only pass in 1, should just return it
+  eq(merged, proj4);
+});
+
+test("merge of 2", ({ eq }) => {
+  const merged = merge(proj4, proj4FullyLoaded);
+  eq(merged.defs["EPSG:32617"], expectation);
+});
+
+test("merge array", ({ eq }) => {
+  const merged = merge([proj4, proj4FullyLoaded]);
+  eq(merged.defs["EPSG:32617"], expectation);
+});
+
+test("merge customized", ({ eq }) => {
+  proj4FullyLoaded.defs("TEST", expectation);
+  const merged = merge([proj4, proj4FullyLoaded]);
+  eq(merged.defs["EPSG:32617"], expectation);
+  eq(merged.defs["TEST"], expectation);
+});
